97adc97ec4c8062a236d1fd9ad02779349d2d570
Messpice <55174034+Messpice@users.noreply.github.com>
2019-09-24 20:07:42 -0700

    Add files via upload

---
+++ escalator_object.py
@@ -0,0 +1,18 @@
+class Escalator():
+    """A simple attempt to model an escalator."""
+    
+    def __init__(self, steps, handrail, motor):
+        """Initialize steps and handrail and motor."""
+        self.steps = steps
+        self.handrail = handrail
+        self.motor = motor
+
+   
+class Pedestrian():
+    """A simple attempt to model a pedestrian."""
+
+    def ___init__(self, gender, name, speed):
+        """Initialize gender and name and speed."""
+        self.gender = gender
+        self.weight = weight
+        self.speed = speed
